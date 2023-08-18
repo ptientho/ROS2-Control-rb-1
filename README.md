525c77c84c8a8951d7679b3aae47afd7eef09ef7
ptientho <ptientho@stevens.edu>
2023-08-18 05:15:40 +0000

    Add a functional lifting unit

--- README.md
+++ README.md
@@ -1,9 +1,55 @@
-# The rb1_ros2_description package
+# Introduction
+This package contains a ROS2 robot model description for Robotnik's RB-1 mobile base with ROS2 controllers. The controllers include
+    1. Differential Drive Controller: control the mobile base of both linear and angular movement
+    2. Forward Command Controller: control the robot's lift up/down
+# Installation
+    Download this package "rb1_ros2_description", which inside the package, installation information can be found in CMakeLists.txt. This package requires the following dependencies.
 
-This package contains a ROS2 robot model description for Robotnik's RB-1 mobile base.   
+        1. ament_cmake
+        2. urdf
+        3. xacro
+        4. robot_state_publisher
+    Also, the following folders inside this package are necessary to be installed.
 
-## Disclaimer:  
-This package only modifies/adapts files from these repositories/packages:  
-- [RobotnikAutomation/rb1_base_sim](https://github.com/RobotnikAutomation/rb1_base_sim) licensed under the BSD 2-Clause "Simplified" License
-- [RobotnikAutomation/rb1_base_common/rb1_base_description](https://github.com/RobotnikAutomation/rb1_base_common/tree/melodic-devel/rb1_base_description), licensed under the BSD License
-- [RobotnikAutomation/robotnik_sensors],(https://github.com/RobotnikAutomation/robotnik_sensors) licensed under the BSD License
+        1. launch
+        2. config
+        3. meshes
+        4. urdf
+        5. xacro
+    
+## Installation Steps
+    1. cd ~/ros2_ws
+    2. colcon build --packages-select rb1_ros2_description
+    3. source install/setup.bash
+# Get start the Simulation
+    Launch the following command after compiling the package.
+
+    ros2 launch rb1_ros2_description rb1_ros2_xacro.launch.py
+
+    This command will launch the mobile base in Gazebo simulation, together with the controllers mentioned above. Wait the gazebo to getup and running.
+# Controller Activation
+    Before controlling the robot in simulation, check if the controllers are up and activated by using the following command in a different shell.
+
+    ros2 control list_controllers
+
+    expected output:
+        forward_effort_controller[forward_command_controller/ForwardCommandController] active
+        joint_state_broadcaster[joint_state_broadcaster/JointStateBroadcaster] active
+        diffbot_base_controller[diff_drive_controller/DiffDriveController] active
+
+    Nore: if not all controllers show up, use ctrl+c to shutdown the nodes and relaunch again.
+
+
+# Moving the robot's lifting unit
+
+    To move the robot's lifting unit, publish to the following topic.
+
+    /forward_effort_controller/commands
+
+    This topic is available when the forward_effort_controller is active. Use ros2 topic list to confirm the availability.
+
+    To publish the topic use the following command.
+
+    ros2 topic pub /forward_effort_controller/commands std_msgs/msg/Float64MultiArray data:\ [1.0]\
+
+    if the number is positive, the lift moves up. Otherwise, the lift moves down.     
--- launch/rb1_ros2_xacro.launch.py
+++ launch/rb1_ros2_xacro.launch.py
@@ -1,18 +1,24 @@
 import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.substitutions import Command, LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from ament_index_python.packages import get_package_prefix
 from launch_ros.descriptions import ParameterValue
+from launch.actions import RegisterEventHandler, TimerAction, GroupAction, ExecuteProcess
+from launch.event_handlers import OnProcessExit
 
 
 def generate_launch_description():
 
+    delay_time_seconds = 20.0
     description_package_name = "rb1_ros2_description"
     install_dir = get_package_prefix(description_package_name)
+
+    world_file_path = "worlds/ros2_online_workshop_v2.world"
+    world_file_pkg = "warehouse_world"
 
     # This is to find the models inside the models folder in rb1_ros2_description package
     gazebo_models_path = os.path.join(description_package_name, 'models')
@@ -34,10 +40,19 @@ def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
+    # Declare a new launch argument for the world file
+    world_file_arg = DeclareLaunchArgument(
+        'world',
+        default_value=[get_package_share_directory(
+            world_file_pkg), world_file_path],
+        description='Path to the Gazebo world file'
+    )
+
     # Define the launch arguments for the Gazebo launch file
     gazebo_launch_args = {
         'verbose': 'false',
         'pause': 'false',
+        'world': LaunchConfiguration('world')
     }
 
    # Include the Gazebo launch file with the modified launch arguments
@@ -46,7 +61,7 @@ def generate_launch_description():
             get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
         launch_arguments=gazebo_launch_args.items(),
     )
-
+    
     # Define the robot model files to be used
     robot_desc_file = "rb1_ros2_base.urdf.xacro"
     robot_desc_path = os.path.join(get_package_share_directory(
@@ -58,21 +73,62 @@ def generate_launch_description():
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
-        namespace=robot_name_1,
         parameters=[{'frame_prefix': robot_name_1+'/', 'use_sim_time': use_sim_time,
-                     'robot_description': ParameterValue(Command(['xacro ', robot_desc_path, ' robot_name:=', robot_name_1]), value_type=str)}],
+                     'robot_description': ParameterValue(Command(['xacro ', robot_desc_path]), value_type=str)}],
         output="screen"
     )
-
+    
     spawn_robot1 = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=['-entity', robot_name_1, '-x', '0.0', '-y', '0.0', '-z', '0.0',
-                   '-topic', robot_name_1+'/robot_description']
+                   '-topic', '/robot_description']
+    )
+    
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    robot_diffdrive_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=["diffbot_base_controller", "-c", "/controller_manager"],
+    )
+
+    robot_lift_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=["forward_effort_controller", "-c", "/controller_manager"],
+    )
+
+    joint = TimerAction(
+        period=40.0,
+        actions=[joint_state_broadcaster_spawner],
     )
 
     return LaunchDescription([
+        world_file_arg,
         gazebo,
         rsp_robot1,
         spawn_robot1,
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_robot1,
+                on_exit=[joint],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[robot_diffdrive_spawner],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=robot_diffdrive_spawner,
+                on_exit=[robot_lift_spawner],
+            )
+        ),
     ])
